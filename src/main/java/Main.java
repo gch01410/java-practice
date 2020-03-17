9a2a5d636106f34f7288bc8cf6d6c81fcdfa9e24
kimminsub <msmk530@naver.com>
2020-03-17 18:09:45 +0900

    코드복사

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+/**
+ * 메인 클래스.
+ */
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hh");
+        /* 게임 시작 */
+        Game game = new Game();
+        game.start();
+
     }
 }
